47aa14778af7c90fcb6357d03884ab54df1f75d1
Nepteine-1 <nepteine@gmail.com>
2025-05-14 20:29:10 +0200

    (feat) create Paint app

---
+++ README.md
@@ -0,0 +1,3 @@
+## OpenCV training
+
+Just a Paint application with adjustable color and brush radius using trackbars
--- main.py
+++ main.py
@@ -1,18 +1,55 @@
-import cv2
+import numpy as np
+import cv2 as cv
+ 
+drawing = False # true if mouse is pressed
+mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+ix,iy = -1,-1
+ 
+def nothing(x):
+    pass
 
-image = cv2.imread('wallpaper.jpg')
+# mouse callback function
+def draw_circle(event,x,y,flags,param):
+    global ix,iy,drawing,mode
+ 
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+ 
+    elif event == cv.EVENT_MOUSEMOVE:
+        if drawing == True:
+            if mode == True:
+                cv.rectangle(img,(ix,iy),(x,y),(cv.getTrackbarPos('B','image'),cv.getTrackbarPos('G','image'),cv.getTrackbarPos('R','image')),-1)
+            else:
+                cv.circle(img,(x,y),cv.getTrackbarPos('Radius','image'),(cv.getTrackbarPos('B','image'),cv.getTrackbarPos('G','image'),cv.getTrackbarPos('R','image')),-1)
+ 
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        if mode == True:
+            cv.rectangle(img,(ix,iy),(x,y),(cv.getTrackbarPos('B','image'),cv.getTrackbarPos('G','image'),cv.getTrackbarPos('R','image')),-1)
+        else:
+            cv.circle(img,(x,y),cv.getTrackbarPos('Radius','image'),(cv.getTrackbarPos('B','image'),cv.getTrackbarPos('G','image'),cv.getTrackbarPos('R','image')),-1)
 
-if image is None:
-    print("Erreur : Impossible de charger l'image.")
 
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+img = np.zeros((512,512,3), np.uint8)
+cv.namedWindow('image')
+cv.setMouseCallback('image',draw_circle)
 
-blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+cv.createTrackbar('R','image',0,255,nothing)
 
-edges = cv2.Canny(blurred_image, 50, 150)
 
-cv2.imshow('Image Originale', image)
-cv2.imshow('Contours', edges)
+cv.createTrackbar('Radius','image',5,45,nothing)
+ 
+cv.createTrackbar('G','image',0,255,nothing)
+cv.createTrackbar('B','image',0,255,nothing)
+ 
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+while(1):
+    cv.imshow('image',img)
+    k = cv.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k == 27:
+        break
+
+cv.destroyAllWindows()
